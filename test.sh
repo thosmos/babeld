0f7422071245d313bb20454858c5b06452065af7
jkilpatr <jkilpatr@redhat.com>
2017-09-24 10:06:03 -0400

    Add rough testing framework
    In light of recent stupid errors I thought it best to spend some time
    making sure I can easily valiate changes, this isn't a unit testing
    system, it's more an integration testing framework where the behavior
    of the whole program is specified, it's not great but it will do for now

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set +eux
+
+check_deps()
+{
+	if !hash cppcheck 2>/dev/null; then
+		echo "Please install cppcheck"	
+        fi
+}
+
+build_babel()
+{
+        cores=$(grep -c ^processor /proc/cpuinfo)
+	make clean
+	make -j $cores
+}
+
+run_lint()
+{
+	cppcheck --force . > /dev/null
+	echo "Linting successful"
+}
+
+run_integration_tests()
+{
+	pushd tests
+		sudo bash ./multihop-basic.sh &
+                wait
+        popd
+}
+
+check_deps
+build_babel
+run_lint
+run_integration_tests
---
+++ tests/network-lab.sh
@@ -0,0 +1,57 @@
+#!bash
+set +eux
+if [ "$1" == "-f" ]; then
+  input=$(<"$2")
+else
+  stdin=$(cat)
+  input=$stdin
+fi
+set -eux
+
+# clear namespaces
+ip -all netns delete
+
+# add namespaces
+echo "adding nodes"
+for node in $(jq '.nodes | keys[]' <<< "$input")
+do
+  # set up alias for later use
+  alias "n${node:1:-1}"="ip netns exec netlab-${node:1:-1}"  
+  ip netns add "netlab-${node:1:-1}"
+done
+
+# iterate over edges array
+echo "adding edges"
+length=$(jq '.edges | length' <<< "$input")
+for ((i=0; i<$length; i++)); do
+
+  # get names of nodes
+  A=$(jq '.edges['$i'].nodes[0]' <<< "$input")
+  B=$(jq '.edges['$i'].nodes[1]' <<< "$input")
+  A=${A:1:-1}
+  B=${B:1:-1}
+
+  # create veth to link them
+  ip link add "veth-$A-$B" type veth peer name "veth-$B-$A"
+
+  # assign each side of the veth to one of the nodes namespaces
+  ip link set "veth-$A-$B" netns "netlab-$A"
+  ip link set "veth-$B-$A" netns "netlab-$B"
+
+  # add ip addresses on each side
+  ipA=$(jq '.nodes["'$A'"].ip' <<< "$input")
+  ipB=$(jq '.nodes["'$B'"].ip' <<< "$input")
+  ip netns exec "netlab-$A" ip addr add ${ipA:1:-1} dev "veth-$A-$B"
+  ip netns exec "netlab-$B" ip addr add ${ipB:1:-1} dev "veth-$B-$A"
+
+  # bring the interfaces up
+  ip netns exec "netlab-$A" ip link set dev "veth-$A-$B" up
+  ip netns exec "netlab-$B" ip link set dev "veth-$B-$A" up
+
+  # add some connection quality issues
+  AtoB=$(jq '.edges['$i']["->"]' <<< "$input")
+  BtoA=$(jq '.edges['$i']["<-"]' <<< "$input")
+
+  ip netns exec "netlab-$A" tc qdisc add dev "veth-$A-$B" root netem ${AtoB:1:-1}
+  ip netns exec "netlab-$B" tc qdisc add dev "veth-$B-$A" root netem ${BtoA:1:-1}
+done
