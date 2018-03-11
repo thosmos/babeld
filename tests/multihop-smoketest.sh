284545a10f7271c7cdf659bcc63949fd97d4f07d
Stan Drozd <drozdziak1@gmail.com>
2018-03-11 17:42:31 +0100

    Weatherproof the tests:
    This commit tries to make obvious errors easier to catch both in CI and
    manual testing.
    
    Note: multihop-smoketest.sh contains commented out suboptimal routes.
    Even though Babel is capable of converging on the best routes in mere
    seconds, the suboptimal paths in the graph are often incorrect or
    possibly riddled with cycles. The suboptimal routes will get
    uncommented/deleted once the problem is resolved/explained.
    
    tests/multihop-gdb-rtt.sh:
    tests/multihop-gdb.sh:
    * Change netlab-4 fee to 7 - no two prices give the same sum anymore
    
    tests/multihop-smoketest.sh:
    * Extend the node layout to a 4-node diamond
    * Minimize hello and update intervals to speed up convergence
    * Decrease the delay to 5 seconds (12 times quicker, baby!)
    * Strengthen route checking - installed route optimality is now
    precisely verified

--- tests/multihop-smoketest.sh
+++ tests/multihop-smoketest.sh
@@ -34,6 +34,7 @@ cleanup()
   kill -9 $(cat babeld-n1.pid)
   kill -9 $(cat babeld-n2.pid)
   kill -9 $(cat babeld-n3.pid)
+  kill -9 $(cat babeld-n4.pid)
   rm -f babeld-n*
  set -eux
 }
@@ -45,8 +46,8 @@ cleanup
   "nodes": {
     "1": { "ip": "1.0.0.1" },
     "2": { "ip": "1.0.0.2" },
-    "3": { "ip": "1.0.0.3" }
-
+    "3": { "ip": "1.0.0.3" },
+    "4": { "ip": "1.0.0.4" }
 },
   "edges": [
      {
@@ -58,40 +59,107 @@ cleanup
       "nodes": ["2", "3"],
       "->": "loss random 0%",
       "<-": "loss random 0%"
+     },
+     {
+      "nodes": ["3", "4"],
+      "->": "loss random 0%",
+      "<-": "loss random 0%"
+     },
+     {
+      "nodes": ["4", "1"],
+      "->": "loss random 0%",
+      "<-": "loss random 0%"
      }
   ]
 }
 EOF
 
 
+cat << EOF
+         ======== TOPOLOGY ========
+
+     ,---- netlab-2 (price \$10) -----,
+    /                                  \\
+   /                                    \\
+netlab-1 (price \$5)                 netlab-3 (price \$1)
+   \\                                    /
+    \\                                  /
+     \`---- netlab-4 (price \$7) ------\`
+
+EOF
 
 ip netns exec netlab-1 sysctl -w net.ipv4.ip_forward=1
 ip netns exec netlab-1 sysctl -w net.ipv6.conf.all.forwarding=1
-ip netns exec netlab-1 $BABELPATH -I babeld-n1.pid -d 1 -L babeld-n1.log -F 5 -w veth-1-2 &
+ip netns exec netlab-1 $BABELPATH -I babeld-n1.pid -d 1 -L babeld-n1.log -F 5 \
+  -w veth-1-4 -w veth-1-2 -h 1 -H 1 -C "default update-interval 1" -a 0 &
 
 ip netns exec netlab-2 sysctl -w net.ipv4.ip_forward=1
 ip netns exec netlab-2 sysctl -w net.ipv6.conf.all.forwarding=1
-ip netns exec netlab-2 $BABELPATH -I babeld-n2.pid -d 1 -L babeld-n2.log -F 10 -w veth-2-1 -w veth-2-3 &
+ip netns exec netlab-2 $BABELPATH -I babeld-n2.pid -d 1 -L babeld-n2.log -F 10 \
+  -w veth-2-1 -w veth-2-3 -h 1 -H 1 -C "default update-interval 1" -a 0 &
 
 ip netns exec netlab-3 sysctl -w net.ipv4.ip_forward=1
 ip netns exec netlab-3 sysctl -w net.ipv6.conf.all.forwarding=1
-ip netns exec netlab-3 $BABELPATH -I babeld-n3.pid -d 1 -L babeld-n3.log -F 1 -w veth-3-2&
+ip netns exec netlab-3 $BABELPATH -I babeld-n3.pid -d 1 -L babeld-n3.log -F 1 \
+  -w veth-3-2 -w veth-3-4 -h 1 -H 1 -C "default update-interval 1" -a 0 &
 
-sleep 60
+ip netns exec netlab-4 sysctl -w net.ipv4.ip_forward=1
+ip netns exec netlab-4 sysctl -w net.ipv6.conf.all.forwarding=1
+ip netns exec netlab-4 $BABELPATH -I babeld-n4.pid -d 1 -L babeld-n4.log -F 7 \
+  -w veth-4-3 -w veth-4-1 -h 1 -H 1 -C "default update-interval 1" -a 0&
+
+sleep 5
+
+# Rule out obvious Babel message problems
 fail_string "malformed" "babeld-n1.log"
 fail_string "malformed" "babeld-n2.log"
 fail_string "malformed" "babeld-n3.log"
+fail_string "malformed" "babeld-n4.log"
 fail_string "unknown version" "babeld-n1.log"
 fail_string "unknown version" "babeld-n2.log"
 fail_string "unknown version" "babeld-n3.log"
-pass_string "dev veth-1-2 reach" "babeld-n1.log"
-pass_string "dev veth-2-1 reach" "babeld-n2.log"
-pass_string "dev veth-2-3 reach" "babeld-n2.log"
-pass_string "dev veth-3-2 reach" "babeld-n3.log"
-pass_string "nexthop 1.0.0.2" "babeld-n1.log"
-pass_string "nexthop 1.0.0.1" "babeld-n2.log"
-pass_string "nexthop 1.0.0.3" "babeld-n2.log"
-pass_string "nexthop 1.0.0.2" "babeld-n3.log"
+fail_string "unknown version" "babeld-n4.log"
+
+# ============================ PRICE TESTS =====================================
+
+# Note: suboptimal routes are commented out due to ridiculous amount time it
+# takes for them to appear.
+
+pass_string "1.0.0.3/32 from.*price 7 fee 5.*via veth-1-4.*nexthop 1.0.0.4" "babeld-n1.log"
+#pass_string "1.0.0.3/32 from.*price 10 fee 5.*via veth-1-2.*nexthop 1.0.0.2" "babeld-n1.log"
+
+pass_string "1.0.0.4/32 from.*price 0 fee 5.*via veth-1-4.*nexthop 1.0.0.4" "babeld-n1.log"
+#pass_string "1.0.0.4/32 from.*price 11 fee 5.*via veth-1-2.*nexthop 1.0.0.2" "babeld-n1.log"
+
+# netlab-2
+pass_string "1.0.0.1/32 from.*price 0 fee 10.*via veth-2-1.*nexthop 1.0.0.1" "babeld-n2.log"
+#pass_string "1.0.0.1/32 from.*price 8 fee 10.*via veth-2-3.*nexthop 1.0.0.3" "babeld-n2.log"
+
+pass_string "1.0.0.3/32 from.*price 0 fee 10.*via veth-2-3.*nexthop 1.0.0.3" "babeld-n2.log"
+#pass_string "1.0.0.3/32 from.*price 12 fee 10.*via veth-2-1.*nexthop 1.0.0.1" "babeld-n2.log"
+
+pass_string "1.0.0.4/32 from.*price 1 fee 10.*via veth-2-3.*nexthop 1.0.0.3" "babeld-n2.log"
+#pass_string "1.0.0.4/32 from.*price 5 fee 10.*via veth-2-1.*nexthop 1.0.0.1" "babeld-n2.log"
+
+# netlab-3
+pass_string "1.0.0.1/32 from.*price 7 fee 1.*via veth-3-4.*nexthop 1.0.0.4" "babeld-n3.log"
+#pass_string "1.0.0.1/32 from.*price 10 fee 1.*via veth-3-2.*nexthop 1.0.0.2" "babeld-n3.log"
+
+pass_string "1.0.0.2/32 from.*price 0 fee 1.*via veth-3-2.*nexthop 1.0.0.2" "babeld-n3.log"
+#pass_string "1.0.0.2/32 from.*price 12 fee 1.*via veth-3-4.*nexthop 1.0.0.4" "babeld-n3.log"
+
+pass_string "1.0.0.4/32 from.*price 0 fee 1.*via veth-3-4.*nexthop 1.0.0.4" "babeld-n3.log"
+#pass_string "1.0.0.4/32 from.*price 15 fee 1.*via veth-3-2.*nexthop 1.0.0.2" "babeld-n3.log"
+
+# netlab-4
+pass_string "1.0.0.1/32 from.*price 0 fee 7.*via veth-4-1.*nexthop 1.0.0.1" "babeld-n4.log"
+#pass_string "1.0.0.1/32 from.*price 11 fee 7.*via veth-4-3.*nexthop 1.0.0.3" "babeld-n4.log"
+
+pass_string "1.0.0.2/32 from.*price 1 fee 7.*via veth-4-3.*nexthop 1.0.0.3" "babeld-n4.log"
+#pass_string "1.0.0.2/32 from.*price 5 fee 7.*via veth-4-1.*nexthop 1.0.0.1" "babeld-n4.log"
+
+pass_string "1.0.0.3/32 from.*price 0 fee 7.*via veth-4-3.*nexthop 1.0.0.3" "babeld-n4.log"
+#pass_string "1.0.0.3/32 from.*price 15 fee 7.*via veth-4-1.*nexthop 1.0.0.1" "babeld-n4.log"
 
 cleanup
 
